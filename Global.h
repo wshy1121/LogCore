bbc15ce637cf5c9604fbf845a7bf0dd8b3484486
wshy1121 <wshy1121@qq.com>
2014-11-08 11:21:01 +0800

    功能宏顺序整理

--- Global.h
+++ Global.h
@@ -27,42 +27,32 @@ private:
 
 #if !defined(NO_CTIME_CALC)
 #define time_trace_level(level)  CCandy candy(__LINE__, (char *)__FILE__, (char *)__FUNCTION__, level)
+#define time_printf(format, ...)    CBugKiller::InsertTrace(__LINE__, (char *)__FILE__, format, ## __VA_ARGS__)
+#define time_all()    CBugKiller::DispAll()
+#define time_tag(format, ...)  CBugKiller::InsertTag(__LINE__, (char *)__FILE__, format, ## __VA_ARGS__)
+#define time_stack()   CBugKiller::printfMemInfMap()
+#define get_stack(str)	CBugKiller::getStackInfo(str)
+#define time_start()    CBugKiller::start()
+#define time_stop()    CBugKiller::stop()
+#define time_num(num)	 time_printf("num:%d    %d", num, __LINE__)
+#define time_err(num)       time_printf("ERRERRERRERRERRERRERR:%d    %d    %s", (num), __LINE__, __FILE__)
 #define time_untrace()  time_trace_level(0)
 #define time_trace()   time_trace_level(100)
 
-#define time_printf(format, ...)    CBugKiller::InsertTrace(__LINE__, (char *)__FILE__, format, ## __VA_ARGS__)
-#define time_num(num)	 time_printf("num:%d    %d", num, __LINE__)
-#define time_err(num)       time_printf("ERRERRERRERRERRERRERR:%d    %d    %s", (num), __LINE__, __FILE__)
-
-#define time_all()    CBugKiller::DispAll();
-
-#define time_tag(format, ...)  CBugKiller::InsertTag(__LINE__, (char *)__FILE__, format, ## __VA_ARGS__);
-
-
-#define time_stack()   CBugKiller::printfMemInfMap();
-#define get_stack(str)	CBugKiller::getStackInfo(str);
-#define time_start()    CBugKiller::start()
-#define time_stop()    CBugKiller::stop()
-
 #else
 #define time_trace_level()    
-#define time_untrace()    
-#define time_trace()   
-
 #define time_printf(format, ...)      
-#define time_num(num)	   
-#define time_err(num)        
-
-#define time_all()     
-
+#define time_all()    
 #define time_tag(format, ...)   
-
-
 #define time_str(str, len)    
 #define time_stack()
 #define get_stack(str)  
 #define time_start()  
 #define time_stop()  
+#define time_num(num)	   
+#define time_err(num)        
+#define time_untrace()    
+#define time_trace()   
 #endif
 
 
