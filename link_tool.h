49a9c50e21d4e2885f3cbcd19a95c9fc5a6321d5
wshy1121 <wshy1121@qq.com>
2014-11-12 10:35:08 +0800

    解决编译问题

--- link_tool.h
+++ link_tool.h
@@ -2,6 +2,7 @@
 #define __LINK_TOOL_H
 #include "time_calc.h"
 #include <map>
+#include <stddef.h>
 #define container_of(ptr, type, member) ({  \
 const typeof( ((type *)0)->member ) *__mptr = (ptr);   \
 (type *)( (char *)__mptr - offsetof(type,member) );})
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <unistd.h>
 #include "Global.h"
 
 //≤‚ ‘CTimeCalc π”√
